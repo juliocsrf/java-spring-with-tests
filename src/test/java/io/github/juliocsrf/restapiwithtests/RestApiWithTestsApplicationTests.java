db01dbc3c1f472ac7091f81d79c08b8df55a119a
Julio Fonseca <julio.fonseca@foconavia.com.br>
2021-12-15 17:34:29 -0300

    first commit

---
+++ src/test/java/io/github/juliocsrf/restapiwithtests/RestApiWithTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.juliocsrf.restapiwithtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

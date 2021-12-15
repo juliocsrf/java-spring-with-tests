4b140ceb1f824b584d705c14989499c6a32e0ec7
Julio Fonseca <julio.fonseca@foconavia.com.br>
2021-12-15 17:53:29 -0300

    Classe user

--- src/main/java/io/github/juliocsrf/restapiwithtests/RestApiWithTestsApplication.java
+++ src/main/java/io/github/juliocsrf/restapiwithtests/RestApiWithTestsApplication.java
@@ -1,5 +1,6 @@
 package io.github.juliocsrf.restapiwithtests;
 
+import io.github.juliocsrf.restapiwithtests.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/io/github/juliocsrf/restapiwithtests/domain/User.java
@@ -0,0 +1,57 @@
+package io.github.juliocsrf.restapiwithtests.domain;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(Integer id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

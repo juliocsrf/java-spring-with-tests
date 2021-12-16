db4d0d19a5536a883d9ff1298fceae381776fce2
Julio Fonseca <juliocsrmf@gmail.com>
2021-12-16 16:58:36 -0300

    Endpoint findById

---
+++ src/main/java/io/github/juliocsrf/restapiwithtests/resources/UserResource.java
@@ -0,0 +1,18 @@
+package io.github.juliocsrf.restapiwithtests.resources;
+
+import io.github.juliocsrf.restapiwithtests.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(new User(1, "Julio", "juliocsrmf@gmail.com", "123"));
+    }
+}

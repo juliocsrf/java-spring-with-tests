4dcf80d5a4095996d078a2bf5d88461800c95a99
Julio Fonseca <juliocsrmf@gmail.com>
2021-12-18 17:28:45 -0300

    Endpoint FindAll

--- src/main/java/io/github/juliocsrf/restapiwithtests/services/UserService.java
+++ src/main/java/io/github/juliocsrf/restapiwithtests/services/UserService.java
@@ -2,8 +2,11 @@ package io.github.juliocsrf.restapiwithtests.services;
 
 import io.github.juliocsrf.restapiwithtests.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById (Integer id);
+    List<User> findAll();
 
 }
--- src/main/java/io/github/juliocsrf/restapiwithtests/services/impl/UserServiceImpl.java
+++ src/main/java/io/github/juliocsrf/restapiwithtests/services/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import io.github.juliocsrf.restapiwithtests.services.exceptions.ObjectNotFoundEx
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +20,10 @@ public class UserServiceImpl implements UserService {
     public User findById(Integer id) {
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return repository.findAll();
     }
 }

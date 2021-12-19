df21eabb76b62a24aa9eb21a70d027de30ec5684
Julio Fonseca <juliocsrmf@gmail.com>
2021-12-18 21:27:56 -0300

    Teste userService delete - Exception

--- src/test/java/io/github/juliocsrf/restapiwithtests/services/impl/UserServiceImplTest.java
+++ src/test/java/io/github/juliocsrf/restapiwithtests/services/impl/UserServiceImplTest.java
@@ -157,6 +157,19 @@ class UserServiceImplTest {
         verify(repository, times(1)).deleteById(anyInt());
     }
 
+    @Test
+    void DeleteWithObjectNotFoundException() {
+
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
+
+        try {
+            service.delete(ID);
+        } catch (Exception exception) {
+            assertEquals(ObjectNotFoundException.class, exception.getClass());
+            assertEquals(OBJETO_NAO_ENCONTRADO, exception.getMessage());
+        }
+    }
+
     private void startUser() {
         user = new User(ID, NAME, EMAIL, PASSWORD);
         userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
